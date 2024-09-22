25e5b826459eeb07c9f179f1a4fcc7b4fea4d425
NidadavoluNiharika <arkniharika@gmail.com>
2024-09-22 20:54:21 +0530

    using functional component

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 import React from "react";
-import * as ReactDOM from 'react-dom';
+import * as ReactDOM from 'react-dom/client';
 
-    const heading = React.createElement("div", {id:'main'},[React.createElement("h1",{id:"child1"},"You are worthy!"),
-    React.createElement("h1",{id:"child2"},"You are responsible for your life!!")   
-    ]);
-     const jsxHeading = <h1> You are worthy. You alone are responsible for your Life.</h1>
+    const Title = () => {
+        return (<h1> You are worthy</h1>);
+    };
+     const HeadingComponent = () => {
+        return (<div> 
+        <Title/>
+        <h1> You alone are responsible for your Life.</h1>
+        </div>);
+     };
     const root = ReactDOM.createRoot(document.getElementById("root"));
-    root.render(jsxHeading);
+    root.render(<HeadingComponent/>);
