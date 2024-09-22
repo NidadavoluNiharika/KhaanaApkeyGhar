589807f15d7b1177d6c77befd4c1a45a16c38ecd
NidadavoluNiharika <arkniharika@gmail.com>
2024-09-22 20:05:51 +0530

    using jsx

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 import React from "react";
 import * as ReactDOM from 'react-dom';
-const heading = React.createElement("div", {id:'main'},[React.createElement("h1",{id:"child1"},"You are worthy!"),
+
+    const heading = React.createElement("div", {id:'main'},[React.createElement("h1",{id:"child1"},"You are worthy!"),
     React.createElement("h1",{id:"child2"},"You are responsible for your life!!")   
     ]);
+     const jsxHeading = <h1> You are worthy. You alone are responsible for your Life.</h1>
     const root = ReactDOM.createRoot(document.getElementById("root"));
-    root.render(heading);
+    root.render(jsxHeading);
