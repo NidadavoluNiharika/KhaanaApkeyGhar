bf1475368668621e93926cf55bc8c33d072af4ce
NidadavoluNiharika <arkniharika@gmail.com>
2024-09-23 22:22:19 +0530

    UI layout and components to be used

--- app.js
+++ app.js
@@ -1,14 +1,31 @@
 import React from "react";
 import * as ReactDOM from 'react-dom/client';
 
-    const Title = () => {
-        return (<h1> You are worthy</h1>);
+    const Header = () => {
+        return (
+            <div className="header">
+            <div className="logo-container">
+                <img 
+                className="logo"
+                src = "https://pngtree.com/freepng/beautiful-muslimah-chef-cartoon-character-illustration_6863411.html"/>
+            </div>
+            <div className="nav-items">
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact Us</li>
+                <li>Cart</li>
+            </ul>
+            </div>
+            </div>
+        );
     };
-     const HeadingComponent = () => {
-        return (<div> 
-        <Title/>
-        <h1> You alone are responsible for your Life.</h1>
-        </div>);
+     const AppLayout = () => {
+        return (
+            <div className="app">
+            <Header/>
+            </div>
+        );
      };
     const root = ReactDOM.createRoot(document.getElementById("root"));
-    root.render(<HeadingComponent/>);
+    root.render(<AppLayout/>);
