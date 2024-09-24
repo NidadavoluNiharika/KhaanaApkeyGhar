0c100740abb6f066084577e40c54d5161e19de29
NidadavoluNiharika <arkniharika@gmail.com>
2024-09-24 19:55:16 +0530

    header and body layout

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ import * as ReactDOM from 'react-dom/client';
             <div className="logo-container">
                 <img 
                 className="logo"
-                src = "https://pngtree.com/freepng/beautiful-muslimah-chef-cartoon-character-illustration_6863411.html"/>
-            </div>
+                src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvPNMBVqQllPnayfCQqIzD1TnP8YE-dWwQRw&s"/>
+                </div>
             <div className="nav-items">
             <ul>
                 <li>Home</li>
@@ -20,10 +20,40 @@ import * as ReactDOM from 'react-dom/client';
             </div>
         );
     };
-     const AppLayout = () => {
+
+    const Body = () => {
+        return(
+            <div className="body">
+             <div className="search">
+                Search
+             </div>
+             <div className="res-container">
+             <RestaurantCard resName = "Big Bowl" cuisine = "Chinese"/>
+             <RestaurantCard resName = "Taara" cuisine = "South Asian"/>
+             </div>
+
+            </div>
+        );
+    };
+
+    const RestaurantCard = (props)=> {
+        return(
+            <div className="res-card">
+            <img className="res-logo"
+            src ="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/enj3srsnhbltbom2ovvh"/>
+            <h3>{props.resName}</h3>
+            <h4>{props.cuisine}</h4>
+            <h4>4.4 star</h4>
+            <h4>15 minutes</h4>
+            </div>
+        )
+    }
+
+    const AppLayout = () => {
         return (
             <div className="app">
             <Header/>
+            <Body/>
             </div>
         );
      };
